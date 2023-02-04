dca9250f8c3a44043a0c49d87b83d92aa6d7e5ba
Filip Risteski <38330826+Revis1on@users.noreply.github.com>
2023-02-04 16:57:20 +0100

    ver 0.924

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,16 @@ import Head from "./head";
 
 const Page = () => (
   <div className="bg-primary-black overflow-hidden">
-    <Head />
+    <head>
+      <title>KleverBPO - Digital Marketing Company</title>
+
+      <meta name="viewport" content="width=device-width, initial-scale=1," />
+      <meta
+        name="description"
+        content="Boost your online presence and drive more sales with our expert digital marketing services. From SEO to social media, we'll help you reach your target audience and achieve your business goal"
+      />
+      <link rel="icon" href="/favicon.ico" />
+    </head>
     <Navbar />
     <Hero />
     <Explore />
