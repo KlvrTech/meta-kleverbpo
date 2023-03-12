4b02d3dd8618b40f9af26da37b79b83a8cade1e1
Filip Risteski <38330826+Revis1on@users.noreply.github.com>
2023-03-12 14:48:36 +0100

    ver 1.01

--- components/LiveChat.jsx
+++ components/LiveChat.jsx
@@ -4,13 +4,23 @@ const LiveChat = () => {
   return (
     <div>
       <chat-widget
+        style={{
+          "--chat-widget-primary-color": "#A509FF",
+          "--chat-widget-active-color": "#A509FF",
+          "--chat-widget-bubble-color": "#A509FF",
+        }}
         location-id="P3kOwFVjFdjKMt4YAvfQ"
+        use-email-field="true"
         prompt-avatar="https://widgets.leadconnectorhq.com/chat-widget/assets/defaultAvatar.png"
-      ></chat-widget>
+        agency-name="KleverBPO"
+        agency-website=""
+      ></chat-widget>{" "}
       <script
         src="https://widgets.leadconnectorhq.com/loader.js"
         data-resources-url="https://widgets.leadconnectorhq.com/chat-widget/loader.js"
-      ></script>
+      >
+        {" "}
+      </script>
     </div>
   );
 };
